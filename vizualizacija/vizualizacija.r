b404daa62b2025b663c6db2103f8f9e4a83b4af6
Jan <jan.perme@gmail.com>
2016-01-19 17:40:07 +0100

    poskušam dodati zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,28 @@
 # 3. faza: Izdelava zemljevida
+source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
+source("uvoz/uvoz.r", encoding = "UTF-8")
+library(ggplot2)
+library(dplyr)
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
+zemljevid <- uvozi.zemljevid("https://www.sharegeo.ac.uk/download/10672/50/English%20Government%20Office%20Network%20Regions%20(GOR).zip","Regions")
+#zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/GBR_adm_shp.zip","GBR_adm2")
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, zemljevid, "OB_UIME", c("Ankaran", "Mirna"))
+pretvori.zemljevid <- function(zemljevid) {
+  fo <- fortify(zemljevid)
+  data <- zemljevid@data
+  data$id <- as.character(0:(nrow(data)-1))
+  return(inner_join(fo, data, by="id"))}
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+ zemljevid <- pretvori.zemljevid(zemljevid)
+ zemljevid <- apply(zemljevid,2,function(x) gsub("East of England|South East|South West","South",x))
+ zemljevid <- apply(zemljevid,2,function(x) gsub("Greater London Authority","London total",x))
+ zemljevid <- apply(zemljevid,2,function(x) gsub("North East|North West|Yorkshire and The Humber","North",x))
+ zemljevid <- apply(zemljevid,2,function(x) gsub("East Midlands|West Midlands","Midlands and Wales",x))
+ drzave=drzave[c(-5,-6,-7,-9),]
+ drzave$imena<-rownames(drzave)
+
+
+ 
+ ggplot() + geom_polygon(data = drzave %>% right_join(zemljevid, by = c("imena" = "NAME")),aes(x = long, y = lat, group = group, fill = Drop))
+print(zemljevid)
